b96e7693353e151f6096fddd36971d81deae55b2
secret <secretx500@qq.com>
2025-12-08 02:40:57 +0800

    refactor: refactor login, add logout and refreshAccessToken

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,24 +1,37 @@
-import { http } from "@/tools/http";
+import { $goto } from "@/hooks/useGoto";
+import { tokenManager } from "@/tools";
+import { ErrnoEnum, http } from "@/tools/http";
+import { removeTokens } from "@/tools/token-manager";
 import type { LoginFormType, LoginResponseType } from "@/types/auth";
-import { loginZod, loginResponseZod } from "@/validation";
 
-/**
- * @description 登录接口
- * POST /api/auth
- * @param {Object} data -  data
- * @returns {Promise<AxiosResponse<LoginResponseType>>}
- */
-export const login = async (data: LoginFormType): Promise<LoginResponseType> => {
-  return await http.request({
-    url: "/api/auth",
-    method: "POST",
-    data,
+// 登录接口
+export const login = (data: LoginFormType) => http.post<LoginResponseType>("/api/auth", data);
 
-    // 请求登录接口时候的数据验证规则, 由于密码会 md5 然后
-    // 再发送, 所以请求接口时密码的验证规则不需要验证密码
-    reqDataZod: loginZod.omit({ password: true }),
-
-    // 验证接口响应数据, 仅仅是为了调试方便, 完全可以不用验证
-    resBodyZod: loginResponseZod,
-  });
+// refreshToken 也过期了, 需要重新登录
+export const logout = () => {
+  removeTokens();
+  $goto.redirectToLogin();
 };
+
+// 自动刷新 accessToken 接口
+export async function refreshAccessToken() {
+  const refreshToken = tokenManager.getRefreshToken();
+  return http
+    .request({
+      // 标记请求: 告诉拦截器(这个请求)不要自动刷新token, 否则可能导致死循环
+      // 注意修改 axios.d.ts 否则会有 ts 类型错误
+      isRefreshTokenRequest: true,
+      url: "/api/auth/refresh_access_token",
+      method: "POST",
+      data: {
+        refreshToken,
+      },
+    })
+    .then((res) => {
+      if (res.status === ErrnoEnum.Unauthorized) {
+        throw new Error("refresh token is expired");
+      }
+      return res as unknown as string;
+    })
+    .catch(logout);
+}
